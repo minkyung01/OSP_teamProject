58e6176d54490dc7cebdd6bfa45f560ab3965ef2
MinJung <52921222+Minjungh63@users.noreply.github.com>
2021-11-15 21:49:53 +0900

    create project
    add icon image on assets file
    
    update workspace.xml

---
+++ Caffeine_Diary/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image
+} from 'react-native';
+const App = () => {
+ return (
+    <View style={styles.container}>
+    <Image style={styles.image}
+        source={require('./../assets/coffee.png')}
+    />
+    <Text style={styles.text}>Caffeine Diary</Text>
+    </View>
+ );
+};
+const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: '#FFE4C3',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text:{
+        padding: 20,
+        fontSize: 50,
+        fontWeight: '700',
+        color: 'black',
+    },
+    image:{
+      width: '60%',
+      height: '60%',
+    },
+
+})
+export default App;
