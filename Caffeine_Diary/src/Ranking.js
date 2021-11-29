3793778ccc9b6dcf31545898f7d791605b751eaf
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2021-11-29 16:12:56 +0900

    Ranking screen

---
+++ Caffeine_Diary/src/Ranking.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {SafeAreaView, View,Text, StyleSheet, Image} from 'react-native';
+
+const Ranking = () => {
+    return(
+        <SafeAreaView style={styles.container}>
+            
+            <Image style={styles.coffee} source={require('./../assets/coffee.png')} />
+
+            <Text style={styles.title}>이화사랑's achievement today.</Text>
+            <Text style={styles.dropdown}>Change standard</Text>
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FF991C',width:'100%',paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                    <Text style={styles.best}>이화사랑 achieved 80% today</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                     <Text style={styles.rankings}>Today’s Ranking</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                     <Text style={styles.rankings}>Love Caffeine(88%)</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                     <Text style={styles.rankings}>이화사랑(44%)</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                     <Text style={styles.rankings}>Tirrred(15%)</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+            <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
+                <SafeAreaView style={styles.content}>
+                     <Text style={styles.rankings}>Lazy teddy bear(8%)</Text>
+                </SafeAreaView>
+            </SafeAreaView>
+
+
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems:'center',
+        padding: 10,
+    },
+    content: {
+        margin: 10,
+        alignItems: 'flex-start',
+        flex: 1,
+    },
+    coffee: {
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 8,
+        width: '20%',
+        height: '60%',
+    },
+    title: {
+        alignItems: 'center',
+        color: '#000000',
+        fontSize: 40,
+        fontWeight: '700',
+    },
+    dropdown: {
+        alignItems: 'flex-end',
+        backgroundColor: '#FF991C',
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight:'700',
+    },
+    best: {
+        fontSize:30,
+        fontWeight:'700',
+        color:'#FFFFFF',
+        flex: 3,
+    },
+    rankings: {
+        fontSize:23,
+        fontWeight:'700',
+        color:'#FFFFFF',
+    },
+})
+
+export default Ranking;
