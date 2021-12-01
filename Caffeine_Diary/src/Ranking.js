20ff73a336985b327968f342dc59cb6af6fc4df4
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2021-12-01 17:56:48 +0900

    edit the layout of Ranking page

--- Caffeine_Diary/src/Ranking.js
+++ Caffeine_Diary/src/Ranking.js
@@ -8,12 +8,18 @@ const Ranking = () => {
             <StatusBar barStyle="light-content" style={textStyles.statusbar}/>
             <ScrollView width = {width-20} height = '80%'>
             
-            <Image style={styles.coffee} source={require('./../assets/coffee.png')} />
+            <SafeAreaView style={{flexDirection:'row'}}>
+                <Image style={styles.coffee} source={require('./../assets/coffee.png')} />
+                <SafeAreaView style={{flexDirection:'column'}}>
+                <Text style={styles.title}>이화사랑's</Text>
+                <Text style={styles.text}> achievement today.</Text>
+                </SafeAreaView>
+            </SafeAreaView>
 
-            <Text style={styles.title}>이화사랑's achievement today.</Text>
             <Pressable>
                 <Text style={styles.dropdown}>Change standard</Text>
             </Pressable>
+
             <SafeAreaView style={{alignItems:'center',flexDirection:'column',backgroundColor:'#FF991C',width:'100%',paddingTop:10,paddingBottom:10}}>
                 <SafeAreaView style={styles.content}>
                     <Text style={styles.best}>이화사랑 achieved 80% today</Text>
@@ -50,7 +56,6 @@ const Ranking = () => {
                      <Text style={styles.rankings}>Lazy teddy bear(8%)</Text>
                 </SafeAreaView>
             </SafeAreaView>
-
             </ScrollView>
         </SafeAreaView>
     );
@@ -70,13 +75,24 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10,
         marginBottom: 8,
+        marginTop: 30,
+        paddingTop: 100,
         width: '20%',
         height: '20%',
+        alignItems: 'flex-start',
     },
     title: {
-        alignItems: 'flex-start',
+        alignItems: 'center',
         color: '#000000',
         fontSize: 40,
+        marginTop: 40,
+        marginLeft: 5,
+    },
+    text: {
+        fontSize: 30,
+        marginBottom: 10,
+    },
+    user: { // user 불러와서 Bold체 해주기 위해
         fontWeight: '700',
     },
     dropdown: {
@@ -105,6 +121,8 @@ const styles = StyleSheet.create({
         fontSize:23,
         fontWeight:'700',
         color:'#FFFFFF',
+        paddingTop: 3,
+        paddingBottom: 3,
     },
 })
 
