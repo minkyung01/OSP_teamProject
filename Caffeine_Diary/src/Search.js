df44be12fe02cbab34e92edfc129c9e0462b541e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2021-12-06 18:45:54 +0900

    edit the layout of Search.js

--- Caffeine_Diary/src/Search.js
+++ Caffeine_Diary/src/Search.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import {SafeAreaView, View,Text, StyleSheet, Image, TextInput} from 'react-native';
-import {basicColor} from './colors'
-import Title from './components/Title';
+import {StatusBar, SafeAreaView,Pressable, View,Text, StyleSheet, Image, TextInput,ScrollView, Dimensions} from 'react-native';
+import {basicColor, themeColor} from './colors';
+import { iconImages } from './images';
+import { textStyles } from './styles';
 
 const Search = () => {
+    const width = Dimensions.get('window').width;
     return(
         <SafeAreaView style={styles.container}>
-            <Title />
+            <StatusBar barStyle="light-content" style={textStyles.statusbar}/>
+            <ScrollView width = {width-20} height = '80%'>
+            <SafeAreaView style={{flexDirection:'row',alignItems:'center',margin:10}}>
+                <Image style={styles.coffee} source={require('./../assets/coffee.png')} />
+                <Text style={styles.title}>TO-DO-LIST</Text>
+            </SafeAreaView>
+
+            <SafeAreaView style={{flexDirection: 'row',backgroundColor:themeColor.Orange.dark}}>
+            <Pressable>
+                <Image style={styles.search} source={require('./../assets/searchIcon.png')} />
+            </Pressable>
             <TextInput style={styles.searchBar}
                 placeholder="Search a task">
             </TextInput>
+            </SafeAreaView>
+
             <Text style={styles.text}>search result: 1</Text>
             <SafeAreaView style={{alignItems:'center',flexDirection:'row',backgroundColor:'#FFE4C3',width:'100%',marginTop:10,paddingTop:10,paddingBottom:10}}>
                 <SafeAreaView style={styles.content}>
                      <Text style={styles.result}>Buying milk</Text>
                 </SafeAreaView>
             </SafeAreaView>
-
+            </ScrollView>
             
         </SafeAreaView>
     );
@@ -29,13 +43,23 @@ const styles = StyleSheet.create({
     },
     content: {
         margin: 10,
-        alignItems: 'flex-start',
+        alignItems: 'center',
         flex: 1,
     },
     coffee: {
+        marginLeft: 10,
+        marginRight: 10,
         marginBottom: 8,
         width: 60,
         height: 90,
+    },
+    search: {
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 8,
+        marginTop: 8,
+        width: 40,
+        height: 40,
     },
     title: {
         alignItems: 'center',
@@ -51,6 +75,7 @@ const styles = StyleSheet.create({
         fontWeight: '700',
     },
     searchBar: {
+        flex: 1,
         backgroundColor: '#FF991C',
         color: '#FFFFFF',
         fontSize: 18,
