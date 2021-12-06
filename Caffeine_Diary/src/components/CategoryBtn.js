814c93a35e2e79b1695515d745fbcaaeab546a3b
MinJung <52921222+Minjungh63@users.noreply.github.com>
2021-12-06 22:52:22 +0900

    SkinColor 적용

--- Caffeine_Diary/src/components/CategoryBtn.js
+++ Caffeine_Diary/src/components/CategoryBtn.js
@@ -2,27 +2,18 @@ import React,{useState, useContext} from 'react';
 import InfoContext from './InfoContext';
 import {Pressable, Text, SafeAreaView, StyleSheet} from 'react-native';
 import {basicColor, themeColor} from '../colors';
-import {Picker} from '@react-native-picker/picker';
 
 const CategoryBtn = ({category}) =>{
     const userContext = useContext(InfoContext);
-    const [item,setItem] = useState(category);
     return(
-        <SafeAreaView style={[styles.button,{backgroundColor:userContext.SkinColor.light}]}>
-                    <Picker
-                      style={{width:160}}
-                      selectedValue={item}
-                      onValueChange={(val,idx)=> {
-                        setItem(val);}
-                      }>
-                        <Picker.Item style={styles.buttonText} label="Today" value={"today"} />
-                        <Picker.Item style={styles.buttonText} label="Assignment" value={"assignment"} />
-                        <Picker.Item style={styles.buttonText} label="Lecture" value={"lecture"} />
-                        <Picker.Item style={styles.buttonText} label="Hobby" value={"hobby"} />
-                        <Picker.Item style={styles.buttonText} label="etc." value={"etc."} />
-                        <Picker.Item style={styles.buttonText} label="Completed" value={"completed"} />
-                        <Picker.Item style={styles.buttonText} label="Uncompleted" value={"uncompleted"} />
-                    </Picker>
+        <SafeAreaView style={[styles.button,{backgroundColor:userContext.SkinColor.dark}]}>
+        <Pressable
+            onPress={()=>{
+                alert("category!!");
+            }}
+        >
+            <Text style={styles.buttonText}>{category}</Text>
+        </Pressable>
         </SafeAreaView>
     );
 };
@@ -30,7 +21,7 @@ const CategoryBtn = ({category}) =>{
 
 const styles = StyleSheet.create({
     button: {
-        width: 160,
+        width: 80,
         height: 40,
         borderRadius: 10,
         marginLeft: 40,
@@ -41,7 +32,7 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 15,
         fontWeight: '700',
-        color: basicColor.text,
+        color: basicColor.background,
     },
 });
 
