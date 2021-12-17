c599ba312c866e1f534c6edba26176db050b5d8f
nugugae <89291329+nugugae@users.noreply.github.com>
2021-12-17 22:45:36 +0900

    Update sharing.js

--- Caffeine_Diary/src/sharing.js
+++ Caffeine_Diary/src/sharing.js
@@ -11,30 +11,6 @@ import {
 } from "react-share";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-<Text onPress={() => onShare()}>이거누르면 공유하기 기능나옴</Text>
-
-const onShare = async () => { 
-    try { 
-        const result = await Share.share( 
-            { 
-                message: link, 
-            } 
-        ); 
-                
-        if (result.action === Share.sharedAction) { 
-            if (result.activityType) { 
-                console.log('activityType!'); 
-            } else { 
-                console.log('Share!'); 
-            } 
-        } else if (result.action === Share.dismissedAction) { 
-                 console.log('dismissed'); 
-        } 
-    } catch (error) {
-             alert(error.message); 
-    } 
-};
-
 const FlexContainer = styled.div`
 	display: flex;
 	flex-direction: column;
